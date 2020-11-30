6084e53884069e85b74a749d81ce78565be2c604
Artur Yakovlev <artur.yakovlev1414@gmail.com>
2020-11-30 18:35:15 +0300

    Fixed java script for signalR chat

--- WebApp/WebApp/wwwroot/js/signalrReqHandler.js
+++ WebApp/WebApp/wwwroot/js/signalrReqHandler.js
@@ -1,14 +1,67 @@
-﻿var connection = new signalR.HubConnectionBuilder()
-    .withUrl('/Home/Index')
-    .build();
+﻿"use strict"
 
-connection.on('receiveMessage', addMessageToChat);
-
-connection.start()
-    .catch(error => {
-        console.error(error.message);
-    });
-
-function sendMessageToHub(message) {
-    connection.invoke('sendMessage', message);
+class Message {
+    constructor(username, text, when) {
+        this.userName = username;
+        this.text = text;
+        this.when = when;
+    }
 }
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl('/ChatHub')
+    .build();
+document.getElementById("submitButton").disabled = true;
+
+const username = document.getElementById('username').value;
+const textinput = document.getElementById('messageText');
+const chat = document.getElementById('chat');
+
+connection.on('receiveMessage', function (message)  {
+    let isCurrentUserMessage = message.userName === username;
+
+    let container = document.createElement('div');
+    container.className = isCurrentUserMessage ? "container darker" : "container";
+
+    let sender = document.createElement('p');
+    sender.className = "sender";
+    sender.innerHTML = message.userName;
+    let text = document.createElement('p');
+    text.innerHTML = message.text;
+
+    let when = document.createElement('span');
+    when.className = isCurrentUserMessage ? "time-left" : "time-right";
+    var currentdate = new Date();
+    when.innerHTML =
+        (currentdate.getMonth() + 1) + "/"
+        + currentdate.getDate() + "/"
+        + currentdate.getFullYear() + " "
+        + currentdate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+
+    container.appendChild(sender);
+    container.appendChild(text);
+    container.appendChild(when);
+    chat.appendChild(container);
+    textinput.value = "";
+})
+
+connection.start().then(function () {
+    document.getElementById("submitButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById('submitButton').addEventListener('click', () => {
+
+    var when = new Date();
+    var message = new Message(username, textinput.value, when);
+    var currentdate = new Date();
+    when.innerHtml =
+        (currentdate.getMonth() + 1) + "/"
+        + currentdate.getDate() + "/"
+        + currentdate.getFullYear() + " "
+        + currentdate.toLocaleString('en-us', { hour: 'numeric', minute: 'numeric', hour12: true });
+
+    connection.invoke("SendMessage", message);
+    event.preventDefault();
+});
